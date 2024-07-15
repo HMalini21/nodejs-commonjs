aa5cbd450b8a0aa81ec66292b9cbed3c165236c9
AMalini21 <hemaarumugam19@gmail.com>
2024-07-15 15:11:58 +0530

    js module

---
+++ app/index.js
@@ -0,0 +1,5 @@
+const { stringlenght, add } = require('../utils/string');
+// const { add } = require('../utils/string');
+console.log('hello world');
+console.log(stringlenght('hello'));
+console.log(add(3, 9));
---
+++ utils/string.js
@@ -0,0 +1,12 @@
+const stringlenght = (str) => {
+  return str.length;
+};
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  stringlenght,
+  add,
+};
